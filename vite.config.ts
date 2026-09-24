11c29aa2fc22d8c0c66ea91a2d58e3a5b05bad9c
Cemal Erkar <cemalerkar4747@gmail.com>
2026-09-24 04:37:06 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pages'de alt dizinlerde (repository adıyla) çalışabilmesi için relative path ayarı eklendi
+  base: '/cerroi/', 
+  build: {
+    outDir: 'docs', // GitHub Pages'in okuyabilmesi için dist yerine docs klasörüne çıkartıyoruz
+    emptyOutDir: true
+  }
 })
